28243f5d587f024508d06f7a84497021d5827943
elihu <elihu.salcedo@gmail.com>
2025-03-25 18:13:54 +0100

    updated cli to parse scan results and metadata

--- gerion_cli/commands/secrets_scan.py
+++ gerion_cli/commands/secrets_scan.py
@@ -1,16 +1,50 @@
 import typer
+from typing_extensions import Annotated
+import json
 from gerion_cli.utils.metadata_utils import get_metadata
 from gerion_cli.utils.secrets_tool_utils import run_secrets_tool
 from gerion_cli.utils.parsing_utils import parse_secrets_tool_output
 
 app = typer.Typer()
 
+# Aux functions
+def save_to_json(results, filename: str):
+    with open(filename, 'w') as json_file:
+        json.dump(results, json_file, indent=4)
+    typer.echo(f"Results saved to {filename}")
+
+def send_to_api(results, api_url: str, api_token: str):
+    import httpx
+    
+    api_url = f'{api_url}/api/client/findings'
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "Content-Type": "application/json"
+    }
+
+    response = httpx.post(api_url, headers=headers, json=json.dumps(results))
+    
+    if response.status_code >= 200 and response.status_code < 300:
+        typer.echo("Data sent to API successfully.")
+    else:
+        typer.echo(f"Failed to send data to API: {response.status_code} - {response.text}")
+
 @app.command()
-def secrets_scan(api_url: str, api_token: str):
+def secrets_scan(
+    code_path: Annotated[str, typer.Argument()] = ".",
+    api_url: str = typer.Option(None, help="API URL for sending results"),
+    api_token: str = typer.Option(None, help="API token for authentication"),
+    output_file: str = typer.Option(None, help="Save results to a JSON file")
+):
     metadata = get_metadata()
-    # print(f'metadata: {metadata}')
-    secrets_tool_output = run_secrets_tool('.')
-    # print(f'secrets_tool_output: {secrets_tool_output}')
-    results = parse_secrets_tool_output(secrets_tool_output)
-    # print(f'results: {results}')
-    typer.echo(results)
+    secrets_tool_output = run_secrets_tool(code_path)
+    results = {'metadata':metadata, 'findings': parse_secrets_tool_output(secrets_tool_output, metadata)}
+
+    if api_url and api_token:
+        send_to_api(results, api_url, api_token)
+    
+    if output_file:
+        save_to_json(results, output_file)
+    
+    if not (api_url or api_token or output_file):
+        typer.echo(results)
--- gerion_cli/utils/parsing_utils.py
+++ gerion_cli/utils/parsing_utils.py
@@ -1,8 +1,71 @@
-def parse_secrets_tool_output(output):
-    # # Parse the output and format it as needed
-    # results = []
-    # for line in output.splitlines():
-    #     if "leak" in line:
-    #         results.append(line)
-    # return "\n".join(results)
-    return output
+import hashlib
+from datetime import datetime
+
+secrets_severity = 'High'
+
+# Aux functions
+def generate_finding_template(metadata):
+    return {
+        'finding_id': None,
+        'title': None,
+        'description': None,
+        'mitigation': None,
+        'severity': None,
+        'security_scope': None,
+        'scan_type': None,
+        'repository_name': metadata['repository_name'],
+        'branch_name': metadata['branch_name'],
+        'build_id': metadata['build_id'],
+        'file_path': None,
+        'line_number': None,
+        'component_name': None,
+        'component_version': None,
+        'active': True,
+        'mitigated': False,
+        'false_positive': False,
+        'creation_date': datetime.now().isoformat(),
+        'last_update_date': datetime.now().isoformat(),
+        'cwe': None,
+        'cve': None,
+        'mitigated_on_build_id': None,
+    }
+
+def generate_unique_id(strings):
+    # Concatenate all strings in the list
+    concatenated_string = ''.join(strings)
+    
+    # Encode the concatenated string to bytes (required for hashing)
+    encoded_string = concatenated_string.encode('utf-8')
+    
+    # Create a SHA-256 hash object
+    sha256_hash = hashlib.sha256()
+    
+    # Update the hash object with the encoded string
+    sha256_hash.update(encoded_string)
+    
+    # Get the hexadecimal representation of the hash and truncate it to 8 characters
+    truncated_hash_hex = sha256_hash.hexdigest()[:12]
+    
+    return truncated_hash_hex
+
+# Tools parsing functions
+def parse_secrets_tool_output(output, metadata):
+    # Parse the output and format it as needed
+    results = []
+    for f in output:
+        template = generate_finding_template(metadata)   
+        result = {
+            'finding_id': str(generate_unique_id([f['RuleID'], f['Description'], str(f['Secret']), f['File']])),
+            'title': f"Hard coded secret: {f['RuleID']}",
+            'description': f['Description'],
+            'mitigation': f"Remove the secret ({f['Secret']}) from the code and rotate it",
+            'severity': secrets_severity,
+            'security_scope': 'Code',
+            'scan_type': 'Secrets',
+            'file_path': f['File'],
+            'line_number': f['StartLine'],
+            'cwe': 798,
+        }
+        results.append({**template, **result})
+        
+    return results
