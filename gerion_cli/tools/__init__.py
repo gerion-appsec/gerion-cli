a764bb5a5ca7e22c7a896ba240a52517ab1c3794
Gerion Dev Team <dev-team@gerion.dev>
2025-06-29 13:15:46 +0200

    Add security tools integration for Gerion CLI
    - Introduced `tools` module containing integrations for Trivy, Gitleaks, and parsing functionalities.
    - Implemented parsing functions for secrets and SCA tool outputs to format findings.
    - Added utility functions for generating unique IDs and finding templates for structured output.
    - Created separate files for each tool's integration and parsing logic, enhancing modularity and maintainability.

---
+++ gerion_cli/tools/__init__.py
@@ -0,0 +1,4 @@
+"""
+Security tools integration for Gerion CLI.
+Contains Trivy, Gitleaks, and parsing functionality.
+""" 
---
+++ gerion_cli/tools/parser.py
@@ -0,0 +1,134 @@
+"""
+Parsing functionality for security tool outputs.
+"""
+import hashlib
+from datetime import datetime
+
+secrets_severity = 'High'
+
+# Aux functions
+def generate_finding_template(metadata):
+    return {
+        'finding_id': None,
+        'title': None,
+        'description': None,
+        'mitigation': None,
+        'severity': None,
+        'security_scope': None,
+        'scan_type': None,
+        'repository_name': metadata['repository_name'],
+        'branch_name': metadata['branch_name'],
+        'build_id': metadata['build_id'],
+        'code_path': metadata['code_path'],
+        'commit_hash': metadata['commit_hash'],
+        'commit_author': metadata['commit_author'],
+        'file_path': None,
+        'line_number': None,
+        'component_name': None,
+        'component_version': None,
+        'component_fix': None,
+        'active': True,
+        'mitigated': False,
+        'false_positive': False,
+        'creation_date': datetime.now().isoformat(),
+        'last_update_date': datetime.now().isoformat(),
+        'cwe': None,
+        'cve': None,
+        'mitigated_on_build_id': None,
+    }
+
+def generate_unique_id(strings):
+    # Concatenate all strings in the list
+    concatenated_string = ''.join(strings)
+    
+    # Encode the concatenated string to bytes (required for hashing)
+    encoded_string = concatenated_string.encode('utf-8')
+    
+    # Create a SHA-256 hash object
+    sha256_hash = hashlib.sha256()
+    
+    # Update the hash object with the encoded string
+    sha256_hash.update(encoded_string)
+    
+    # Get the hexadecimal representation of the hash and truncate it to 8 characters
+    truncated_hash_hex = sha256_hash.hexdigest()[:12]
+    
+    return truncated_hash_hex
+
+# Tools parsing functions
+def parse_secrets_tool_output(output, metadata):
+    """
+    Parses the output of a secrets tool and formats it as a set of findings.
+
+    Args:
+        output: A list of dictionaries containing secret findings.
+        metadata: Metadata about the scan.
+
+    Returns:
+        A list of dictionaries representing the unique findings.
+    """    
+    results = []
+    seen_finding_ids = set()
+    for f in output:
+        template = generate_finding_template(metadata)   
+        finding_id = str(generate_unique_id([f['File'], str(f['Match']), f['RuleID']]))
+        
+        # Check if the finding_id has already been processed
+        if finding_id not in seen_finding_ids:
+            result = {
+                'finding_id': finding_id,
+                'title': f"Hard coded secret: {f['RuleID']}",
+                'description': f['Description'],
+                'mitigation': f"Remove the secret ({f['Secret']}) from the code and rotate it",
+                'severity': secrets_severity,
+                'security_scope': 'Code',
+                'scan_type': 'Secrets',
+                'file_path': f['File'],
+                'line_number': f['StartLine'],
+                'cwe': ['CWE-798'],
+            }
+            results.append({**template, **result})
+            seen_finding_ids.add(finding_id)  # Mark this finding_id as processed
+        
+    return results
+
+def parse_sca_tool_output(output, metadata):
+    """
+    Parses the output of a sca tool and formats it as a set of findings.
+
+    Args:
+        output: A list of dictionaries containing sca findings.
+        metadata: Metadata about the scan.
+
+    Returns:
+        A list of dictionaries representing the unique findings.
+    """    
+    results = []
+    seen_finding_ids = set()
+    for e in output:
+        if 'Vulnerabilities' in e:
+            for f in e['Vulnerabilities']:
+                template = generate_finding_template(metadata)   
+                finding_id = str(generate_unique_id([e['Target'], f['VulnerabilityID'], str(f['PkgID'])]))
+                
+                # Check if the finding_id has already been processed
+                if finding_id not in seen_finding_ids:
+                    result = {
+                        'finding_id': finding_id,
+                        'title': f"{f['VulnerabilityID']} - {f['PkgID']}",
+                        'description': f['Description'],
+                        'mitigation': f"Update the package {f['PkgID']} if it has fix. Status: {f['Status']}. Fixed in: {f['FixedVersion'] if 'FixedVersion' in f else 'No fix'}",
+                        'severity': f['Severity'],
+                        'security_scope': 'Code',
+                        'scan_type': 'SCA',
+                        'file_path': e['Target'],
+                        'component_name': f['PkgName'],
+                        'component_version': f['InstalledVersion'],
+                        'component_fix': f['FixedVersion'] if 'FixedVersion' in f else None,
+                        'cwe': f['CweIDs'] if 'CweIDs' in f else None,
+                        'cve': f['VulnerabilityID']
+                    }
+                    results.append({**template, **result})
+                    seen_finding_ids.add(finding_id)  # Mark this finding_id as processed
+        
+    return results 
